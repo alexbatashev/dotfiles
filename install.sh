a54d97f92af7a2b1988ab46d1fda2930bfc78c4b
Alexander Batashev <alexbatashev@gmail.com>
2021-05-27 21:44:33 +0300

    Add Tumbleweed support

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   $DIR/macos.sh
-fi
-
-if [ -f /etc/redhat-release ]; then
+elif [ -f /etc/redhat-release ]; then
   $DIR/fedora.sh
+else
+  $DIR/tumbleweed.sh
 fi
 
 mkdir -p ~/.config/nvim/
---
+++ tumbleweed.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+sudo zypper install zsh neovim cmake ninja ripgrep perf 
+
+chsh -s $(which zsh)
