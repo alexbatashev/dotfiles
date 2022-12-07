4469c32882bb074d9c2e7d93c31ed71612cd3b33
Alexander Batashev <alexbatashev@outlook.com>
2022-12-07 22:35:35 +0800

    fix some things on macos

--- macos.sh
+++ macos.sh
@@ -3,11 +3,11 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 brew update
-brew tap caskroom/cask
+brew tap homebrew/cask
 
-brew cask install visual-studio-code
-brew cask install iterm2
-brew cask install docker
+brew install --cask visual-studio-code
+brew install --cask iterm2
+brew install --cask docker
 brew install nvim
 brew install zsh
 brew install ripgrep exa bat
@@ -15,3 +15,7 @@ brew install nodejs
 brew install wget
 brew install cmake ninja
 brew install antibody
+
+wget -O /tmp/jetbrainsmono.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip
+unzip -d /tmp /tmp/jetbrainsmono.zip
+sudo mv /tmp/*.ttf /Library/Fonts
