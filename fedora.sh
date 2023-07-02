470d0373710dfbd0d192d7a60c134d52e150ff99
Alexander Batashev <alexbatashev@outlook.com>
2023-07-02 18:53:04 +0300

    install fzf into fedora

--- fedora.sh
+++ fedora.sh
@@ -9,6 +9,7 @@ sudo dnf install -y wget
 sudo dnf install -y util-linux-user
 sudo dnf install -y python3 python3-pip
 sudo dnf install -y fd-find
+sudo dnf install -y fzf
 
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
