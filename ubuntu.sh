92a5bb7507fe6ae4092162f44f6111f64326ce14
Alexander Batashev <alexbatashev@gmail.com>
2023-07-08 19:46:22 +0300

    add ripgrep to ubuntu

--- ubuntu.sh
+++ ubuntu.sh
@@ -5,7 +5,7 @@ sudo apt install -yqq curl zsh neovim build-essential \
                       cmake ninja-build wget linux-tools-generic \
                       tmux exa bat fd-find clang lldb gdb nodejs \
 		      tre-command git-extras python3 python-is-python3 \
-                      python3-pip fzf python3-venv gettext
+                      python3-pip fzf python3-venv gettext ripgrep
 
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 
