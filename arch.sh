e0768ffe44054aca8caa58e81b1c2f8736b23c7f
Alexander Batashev <alexbatashev@outlook.com>
2024-11-30 21:25:14 +0300

    add btop to arch

--- arch.sh
+++ arch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 sudo pacman -S yay || echo "Failed to install yay"
-sudo pacman -S zsh neovim cmake ninja ripgrep wget perf tmux exa bat fd zoxide fzf github-cli brave-browser 
+sudo pacman -S zsh neovim cmake ninja ripgrep wget perf tmux exa bat fd zoxide fzf github-cli brave-browser btop 
 sudo pacman -S tre-command || echo "Failed to install tre-command"
 
 sudo pacman -S clang lldb gdb openmp lld libc++
