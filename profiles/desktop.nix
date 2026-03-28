a4031a5899199cb68a4078a7a9875b502affe5ea
Alexander Batashev <alex@batashev.me>
2026-03-28 11:21:36 +0300

    update deps

--- profiles/desktop.nix
+++ profiles/desktop.nix
@@ -3,8 +3,8 @@
   targets.genericLinux.enable = true;
   targets.genericLinux.gpu.nvidia = {
     enable = true;
-    version = "590.48.01";
-    sha256 = "sha256-ueL4BpN4FDHMh/TNKRCeEz3Oy1ClDWto1LO/LWlr1ok=";
+    version = "595.45.04";
+    sha256 = "sha256-zUllSSRsuio7dSkcbBTuxF+dN12d6jEPE0WgGvVOj14=";
   };
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.nvidia.acceptLicense = true;
