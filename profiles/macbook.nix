630f5353182c04644b3d37ebc8138b9a37354d49
Alexander Batashev <alex@batashev.me>
2026-04-18 18:41:31 +0300

    fix macos stuff

--- profiles/macbook.nix
+++ profiles/macbook.nix
@@ -10,6 +10,12 @@
     "zed.cachix.org-1:/pHQ6dpMsAZk2DiP4WCL0p9YDNKWj2Q5FL20bNmw1cU="
     "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
   ];
+  nix.settings.trusted-users = [
+    "root"
+    "alex"
+    "@admin"
+  ];
+  nix.settings.builders-use-substitutes = true;
 
   users.users.${username}.home = "/Users/${username}";
 
