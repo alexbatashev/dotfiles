5c2573eb7b2113e73f92e32adb890c63a9f00e6e
Alexander Batashev <alex@batashev.me>
2026-07-25 10:54:59 +0300

    update nvidia driver

--- profiles/desktop.nix
+++ profiles/desktop.nix
@@ -3,8 +3,8 @@
   targets.genericLinux.enable = true;
   targets.genericLinux.gpu.nvidia = {
     enable = true;
-    version = "610.43.02";
-    sha256 = "sha256-MDSgVLtM33dS/43CclZMsQVROAS/9TU4lFkBsWyndGM=";
+    version = "610.43.03";
+    sha256 = "sha256-ReLUwTSiPDXlDyU6SqY+fl6NF+PRhdSgfIpY6WEu05I=";
   };
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.nvidia.acceptLicense = true;
