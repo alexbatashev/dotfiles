d4a72a21af70e048414426cc6bb81ce10babe75c
Alexander Batashev <alex@batashev.me>
2026-08-15 14:55:33 +0300

    update flakes

--- flake.nix
+++ flake.nix
@@ -22,17 +22,6 @@
     nixgl.url = "github:nix-community/nixGL";
   };
 
-  nixConfig = {
-    extra-substituters = [
-      "https://zed.cachix.org"
-      "https://cache.garnix.io"
-    ];
-    extra-trusted-public-keys = [
-      "zed.cachix.org-1:/pHQ6dpMsAZk2DiP4WCL0p9YDNKWj2Q5FL20bNmw1cU="
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-    ];
-  };
-
   outputs =
     {
       self,
